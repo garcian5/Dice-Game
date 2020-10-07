eae28d03b01a49e3b4d29cf04729893c6b96483e
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-10-07 10:38:21 -0600

    forgot to display none to the 2nd dice after game over

--- app.js
+++ app.js
@@ -77,6 +77,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         if(scores[activePlayer] >= winningScore()){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.dice2').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
